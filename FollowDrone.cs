539758a77b923659b0717e5f04ba511613bc2f6c
Suzy Adel Wahba <128175020+SuzyAdel@users.noreply.github.com>
2025-05-11 19:25:33 +0300

    snow particle following drone

---
+++ FollowDrone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowDrone : MonoBehaviour
+{
+    public Transform drone;      // Assign your drone GameObject here
+    public Vector3 offset;       
+
+    void LateUpdate()
+    {
+        if (drone != null)
+        {
+            transform.position = drone.position + offset;
+        }
+    }
+}
